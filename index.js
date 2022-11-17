09a8b888da1264f3523d8e4755b7b0a8d4b1ff67
이상철 <zlrz6706@gmail.com>
2022-11-17 18:20:35 +0900

    회원가입기능

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 const express = require('express'); // 익스프레스 모듈을 가져온다.
 const app = express() // 새로운 익스프레스 앱을 만들고 !
 const port = 3000 // 포트는 내 마음 !
+const {User} = require("./models/User");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true})); // 바디파서에 옵션을 주기 위해 사용합니다.
+// => 위 코드는 application/x-www-form-urlencoded 의 형식의 데이터를 읽어주고
+app.use(bodyParser.json());
+// => 위 코드는 application/json 형식의 데이터를 읽는다.
 
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://zlrz002:tkdas6708%40@boilerian.jklar1o.mongodb.net/?retryWrites=true&w=majority", {
@@ -12,6 +19,25 @@ mongoose.connect("mongodb+srv://zlrz002:tkdas6708%40@boilerian.jklar1o.mongodb.n
   .catch(err => console.log(err))
 
 app.get("/", (request, response) => response.send("Hello World How are you ~?"))
+
+//회원가입 라우터 기능 !
+app.post("/register", (request, response) => {
+
+  // 회원가입시 필요한 정보들을 클라이언트에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  // 1. 인스턴스 만들기
+  const user = new User(request.body)
+
+  // 2. 몽고디비 메서드인 save를 이용해서 정보들이 유저모델에 저장이된다. 이후 콜백함수가 오는데 이때 에러가 있으면 클라이언트에 에러가 있다고 제이슨 형식으로 전달해준다.
+  user.save((err, userInfo) => {
+    if(err) return response.json({ success: false, err })
+    return response.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
